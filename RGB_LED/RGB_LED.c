6f2c452f567cb41e95bd9bd0c1f6b5b346862673
M.Khlmann <toBeEdited@edit.com>
2024-05-16 21:33:09 +0200

    Color Get Function, Enable Function

--- RGB_LED/RGB_LED.c
+++ RGB_LED/RGB_LED.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 #include <driver/ledc.h>
 #include "RGB_LED.h"
+#include "esp_log.h"
 
 /*
 #define-Section -- Set up for ESP32C3
 
-Change to customize 
+Change to fit your system 
 */
 #define LEDC_TIMER_NUM 1
 #define LEDC_R_CHANNEL 0
@@ -36,7 +38,11 @@ rgb_color RGB_COLOR_BLUE = {
     .blue = 255
 };
 
-//Configuration for 3 PWM Channels (set up for ESP32C3)
+unsigned char rgb_current[3] = {};
+
+/*
+Configuration for 3 PWM Channels (set up for ESP32C3, change to fit your system)
+*/
 ledc_channel_config_t ledc_channel[LEDC_CHANNEL_AMNT] = {
     {
         .channel = LEDC_R_CHANNEL,
@@ -67,7 +73,9 @@ ledc_channel_config_t ledc_channel[LEDC_CHANNEL_AMNT] = {
     }
 };
 
-//Setup-Function -- run first before using other functions
+/*
+Setup-Function -- run first before using other functions
+*/
 void rgb_setup(unsigned char gpio_r, unsigned char gpio_g, unsigned char gpio_b)
 {
     ledc_timer_config_t ledc_timer = {
@@ -88,6 +96,26 @@ void rgb_setup(unsigned char gpio_r, unsigned char gpio_g, unsigned char gpio_b)
     }
 
 }
+/*
+Enables rgb with last used color values
+*/
+void rgb_enable(void) {
+    for (int i_enable = 0; i_enable < LEDC_CHANNEL_AMNT; i_enable++) {
+        ledc_set_duty(ledc_channel[i_enable].speed_mode, ledc_channel[i_enable].channel, rgb_current[i_enable]);
+        ledc_update_duty(ledc_channel[i_enable].speed_mode, ledc_channel[i_enable].channel);
+    }
+    
+}
+
+/*
+Disables RGB LED Strip by setting duty cycle to 0
+*/
+void rgb_disable(void) {
+    for (int i_disable = 0; i_disable < LEDC_CHANNEL_AMNT; i_disable++) {
+        ledc_set_duty(ledc_channel[i_disable].speed_mode, ledc_channel[i_disable].channel, 0);
+        ledc_update_duty(ledc_channel[i_disable].speed_mode, ledc_channel[i_disable].channel);    
+    }
+}
 
 /*
 Sets RGB LED Strip to specific color by adjusting duty cicle to value (0-255)
@@ -100,10 +128,12 @@ Arguments:
 void rgb_set_color(unsigned char red_value, unsigned char green_value, unsigned char blue_value) {
     
     unsigned char colorArray[3] = {red_value, green_value, blue_value};
+    memcpy(rgb_current,colorArray,sizeof(colorArray));
 
     for (int i_color = 0; i_color < LEDC_CHANNEL_AMNT; i_color++) {
         ledc_set_duty(ledc_channel[i_color].speed_mode, ledc_channel[i_color].channel, colorArray[i_color]);
         ledc_update_duty(ledc_channel[i_color].speed_mode, ledc_channel[i_color].channel);
+
     }
     
 }
@@ -118,17 +148,19 @@ void rgb_set_color_array(unsigned char color[3]) {
     
     for (int a = 0; a < LEDC_CHANNEL_AMNT; a++) {
         ledc_set_duty(ledc_channel[a].speed_mode, ledc_channel[a].channel, color[a]);
-        ledc_update_duty(ledc_channel[a].speed_mode, ledc_channel[a].channel);    
+        ledc_update_duty(ledc_channel[a].speed_mode, ledc_channel[a].channel);
+
+        rgb_current[a] = color[a];    
     }
     
 }
 
 /*
-Sets RGB LED Strip to specific color by adjusting duty cicle to value (0-255)
-Use with predefined color structs. 
+Sets RGB LED Strip to specific color by adjusting duty cicle to value (0-255).
+Intended to use with predefined color structs. 
 
 Arguments:
-rgb_color color: struct of type rgb_color
+- rgb_color color: struct of type rgb_color
 */
 void rgb_set_predefined_color(rgb_color color) {
     //Pointer zu ersten Element in color struct
@@ -137,15 +169,24 @@ void rgb_set_predefined_color(rgb_color color) {
     for(unsigned char i_struct = 0; i_struct < LEDC_CHANNEL_AMNT; i_struct++) {
         ledc_set_duty(ledc_channel[i_struct].speed_mode, ledc_channel[i_struct].channel, *colorPtr); //Wert mit Pointer eingesetzt
         ledc_update_duty(ledc_channel[i_struct].speed_mode, ledc_channel[i_struct].channel);
+
+        rgb_current[i_struct] = *colorPtr;
+
         //Adresse in Pointer um 1 erhöht
         colorPtr++;
     }
 }
 
-//Disables RGB LED Strip by setting duty cycle to 0
-void rgb_disable(void) {
-    for (int b = 0; b < LEDC_CHANNEL_AMNT; b++) {
-        ledc_set_duty(ledc_channel[b].speed_mode, ledc_channel[b].channel, 0);
-        ledc_update_duty(ledc_channel[b].speed_mode, ledc_channel[b].channel);    
+/*
+Returns current color/duty values as rgb_color struct 
+*/
+rgb_color rgb_get_color() {
+    rgb_color currentColor = {};
+    unsigned char *currentColorPtr = (unsigned char*)&currentColor;
+
+    for(int i_get = 0; i_get < LEDC_CHANNEL_AMNT; i_get++) {
+        *currentColorPtr = (unsigned char)ledc_get_duty(ledc_channel[i_get].speed_mode, ledc_channel[i_get].channel);
+        currentColorPtr++;
     }
+    return currentColor;
 }
--- RGB_LED/include/RGB_LED.h
+++ RGB_LED/include/RGB_LED.h
@@ -12,10 +12,13 @@ extern rgb_color RGB_COLOR_BLUE;
 
 void rgb_setup(unsigned char gpio_r, unsigned char gpio_g, unsigned char gpio_b);
 
+void rgb_enable(void);
+void rgb_disable(void);
+
 void rgb_set_color(unsigned char red_value, unsigned char green_value, unsigned char blue_value); 
 void rgb_set_color_array(unsigned char color[3]);
 void rgb_set_predefined_color(rgb_color color);
 
-void rgb_disable(void);
+rgb_color rgb_get_color();
 
 #endif
